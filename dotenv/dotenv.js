5f29b844a5e8684410f3eca487d8350e76227588
lilithkarapetyan <lilith.karapetyann@gmail.com>
2020-09-24 19:23:52 +0400

    format validation + removed n-readlines lib

--- dotenv/dotenv.js
+++ dotenv/dotenv.js
@@ -1,4 +1,4 @@
-const lineByLine = require('n-readlines');
+const fs = require('fs')
 const config = (path = './.env') => {
     const ret = {
         parsed: {},
@@ -6,19 +6,24 @@ const config = (path = './.env') => {
     };
 
     try {
-        const liner = new lineByLine(path);
+        const fd = fs.openSync(path, 'r');
+        const bufferSize = 256;
+        const buffer = Buffer.alloc(bufferSize);
 
-        let line;
-        while (line = liner.next()) {
-            if (line) {
-                line = line.toString().replace(/\r?\n|\r/g, '');
-                const [key, value] = line.split('=');
-                if (key && value) {
-                    process.env[key] = value;
-                    ret.parsed[key] = value;
-                }
+        let leftOver = '';
+        let read, line, indexStart, index, lineNumber = 1;
+        while ((read = fs.readSync(fd, buffer, 0, bufferSize, null)) !== 0) {
+            indexStart = 0;
+            leftOver += buffer.toString('utf8', 0, read);
+            while ((index = leftOver.indexOf('\n', indexStart)) !== -1) {
+                line = leftOver.substring(indexStart, index, lineNumber);
+                _processLine(line, ret, lineNumber);
+                indexStart = index + 1;
+                lineNumber++;
             }
+            leftOver = leftOver.substring(indexStart);
         }
+        _processLine(leftOver, ret, lineNumber);
 
         delete ret.error;
     } catch (err) {
@@ -26,6 +31,29 @@ const config = (path = './.env') => {
         delete ret.parsed;
     }
     return ret;
+};
+
+const _processLine = (line, ret, lineNumber) => {
+    if (line && !line.startsWith('#')) {
+
+        if (!line.includes('=')) {
+            console.log('\x1b[31m', `Invalid Format on line ${lineNumber}: The correct format is KEY=VALUE`, '\x1b[0m');
+            return;
+        }
+
+        line = line.replace(/\r?\n|\r/g, '').trim();
+        const [key, value] = line.split('=');
+        if (key) {
+
+            if (process.env[key]) {
+                console.log('\x1b[33m', `Warning: Overriding the variable '${key}' \x1b[0m`);
+            }
+
+            process.env[key] = value || '';
+            ret.parsed[key] = value || '';
+        }
+
+    }
 };
 
 module.exports = {
