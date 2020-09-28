8053f2a99d42fcaf3783ef932f2876eb8bed1f0e
lilithkarapetyan <lilith.karapetyann@gmail.com>
2020-09-28 19:08:41 +0400

    changed to the traditional promises

--- show-files/index.js
+++ show-files/index.js
@@ -2,35 +2,30 @@ const fs = require('fs').promises;
 const pathModule = require('path');
 const path = process.argv.slice(2)[0];
 
-const processDir = async (path, parent) => {
-    try {
-        const names = await fs.readdir(path);
-        const statPromises = names.map(name => fs.lstat(pathModule.join(path, name)));
-        const stats = await Promise.all(statPromises);
-        const subDirPromise = stats.map(stat => {
-            const name = names[stats.indexOf(stat)]
-            if (stat.isDirectory()) {
-                if (!parent[name])
-                    parent[name] = {};
-                return processDir(pathModule.join(path, name), parent[name]);
-            }
-            parent[name] = true;
-            return name;
-        });
-        await Promise.all(subDirPromise);
-    }
-    catch (err) {
-        console.log(err)
-    }
+const processDir = (path, parent) => {
+    return new Promise((res, rej) => {
+        fs.readdir(path).then( names => {
+            const statPromises = names.map(name => fs.lstat(pathModule.join(path, name)));
+            Promise.all(statPromises).then(stats => {
+                const subDirPromise = stats.map(stat => {
+                    const name = names[stats.indexOf(stat)]
+                    if (stat.isDirectory()) {
+                        if (!parent[name])
+                            parent[name] = {};
+                        return processDir(pathModule.join(path, name), parent[name]);
+                    }
+                    parent[name] = true;
+                    return name;
+                })
+                Promise.all(subDirPromise).then((data) => {
+                    res(data)
+                }).catch(rej);
+            }).catch(rej);
+        }).catch(rej);
+    });
 }
 
-(async () => {
-    try {
-        const result = {};
-        await processDir(path, result)
-        console.log(result)
-    }
-    catch (err) {
-        console.log(err);
-    }
-})();
+const result = {};
+processDir(path, result).then(() => {
+    console.log(result)
+})
