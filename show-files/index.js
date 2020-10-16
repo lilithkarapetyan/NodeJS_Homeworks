138d44508f128502afcf3d87c2565fe5c918d4c4
lilithkarapetyan <lilith.karapetyann@gmail.com>
2020-10-17 01:15:23 +0400

    chained promises

--- show-files/index.js
+++ show-files/index.js
@@ -4,9 +4,9 @@ const path = process.argv.slice(2)[0];
 
 const processDir = (path, parent) => {
     return new Promise((res, rej) => {
-        fs.readdir(path).then( names => {
+        return fs.readdir(path).then( names => {
             const statPromises = names.map(name => fs.lstat(pathModule.join(path, name)));
-            Promise.all(statPromises).then(stats => {
+            return Promise.all(statPromises).then(stats => {
                 const subDirPromise = stats.map(stat => {
                     const name = names[stats.indexOf(stat)]
                     if (stat.isDirectory()) {
@@ -17,15 +17,15 @@ const processDir = (path, parent) => {
                     parent[name] = true;
                     return name;
                 })
-                Promise.all(subDirPromise).then((data) => {
+                return Promise.all(subDirPromise).then((data) => {
                     res(data)
-                }).catch(rej);
-            }).catch(rej);
+                });
+            });
         }).catch(rej);
     });
 }
 
 const result = {};
 processDir(path, result).then(() => {
-    console.log(result)
+    console.log(JSON.stringify(result))
 })
