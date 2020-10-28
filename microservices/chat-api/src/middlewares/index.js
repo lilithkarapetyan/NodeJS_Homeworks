ca776a1c30840e00ed6135a2fc54d6e9c30185a0
Lilit Karapetyan <lilitkarapetyan@Lilits-MacBook-Pro.local>
2020-10-28 19:14:42 +0400

    added export

--- microservices/chat-api/src/middlewares/index.js
+++ microservices/chat-api/src/middlewares/index.js
@@ -1,7 +1,9 @@
 const authMiddleware = require('./auth-middleware');
 const validateUserMiddleware = require('./validate-user-middleware');
+const errorHandlerMiddleware = require('./error-handler-middleware');
 
 module.exports = {
     authMiddleware,
     validateUserMiddleware,
+    errorHandlerMiddleware,
 };
